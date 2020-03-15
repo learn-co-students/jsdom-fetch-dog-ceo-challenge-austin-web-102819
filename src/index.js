5e85e556e9176a3d45947072fe15108c4c1ff147
Farmwell <g.ferowich@gmail.com>
2020-03-15 17:09:29 -0500

    dom manipulation practice

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
-console.log('%c HI', 'color: firebrick')
+console.log("%c HI", "color: firebrick");
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM is loaded");
+  fetchDogs();
+  fetchBreeds();
+});
+
+const breedUrl = "https://dog.ceo/api/breeds/list/all";
+const fetchBreeds = () => {
+  fetch(breedUrl)
+    .then(response => response.json())
+    .then(data => populateBreedList(data.message));
+};
+
+const populateBreedList = data => {
+  const breedList = document.getElementById("dog-breeds");
+
+  for (x in data) {
+    let dog = document.createElement("li");
+    dog.innerText = x;
+    breedList.appendChild(dog);
+    dog.style.cursor = "pointer";
+    dog.addEventListener("click", addColorEffect);
+  }
+};
+
+const addColorEffect = event => {
+  event.target.style.color = "green";
+};
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+const fetchDogs = () => {
+  fetch(imgUrl)
+    .then(response => response.json())
+    .then(data => addDogImages(data.message));
+};
+
+const addDogImages = data => {
+  data.forEach(dogImgUrl => {
+    let dogsContainer = document.getElementById("dog-image-container");
+    let DOM_img = document.createElement("img");
+    DOM_img.src = dogImgUrl;
+    dogsContainer.appendChild(DOM_img);
+  });
+};
